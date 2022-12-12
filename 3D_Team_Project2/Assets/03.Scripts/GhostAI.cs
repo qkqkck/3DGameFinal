89759a5cec68b959187e3b4e6424e14bce9a15ef
RynT <clamper1223@naver.com>
2022-12-13 04:24:10 +0900

    collision
    충돌처리만

--- 3D_Team_Project2/Assets/03.Scripts/GhostAI.cs
+++ 3D_Team_Project2/Assets/03.Scripts/GhostAI.cs
@@ -12,8 +12,8 @@ public class GhostAI: MonoBehaviour
     private NavMeshAgent nvAgent;
     private Animator animator;
 
-    public float traceDist = 100.0f;
-    public float attackDist = 2.0f;
+    public float traceDist = 10f;
+    public float attackDist = 0.1f;
     private bool isDie = false;
 
     //private int hp = 100;
@@ -26,7 +26,7 @@ public class GhostAI: MonoBehaviour
         animator = this.gameObject.GetComponent<Animator>();
 
         StartCoroutine(this.CheckMonsterState());
-        StartCoroutine(this.MosterAction());
+        //StartCoroutine(this.MosterAction());
     }
 
     IEnumerator CheckMonsterState()
@@ -42,85 +42,11 @@ public class GhostAI: MonoBehaviour
                 //monsterState = MonsterState.attack;
                 GameObject.Find("Player").GetComponent<PlayerCtrl>().IsDie();
             }
-            else if (dist <= traceDist)
-            {
-                monsterState = MonsterState.trace;
-            }
-            else
-            {
-                monsterState = MonsterState.idle;
-            }
+            
         }
     }
 
-    IEnumerator MosterAction()
-    {
-        while (!isDie)
-        {
-            switch (monsterState)
-            {
-                case MonsterState.idle:
-                    nvAgent.isStopped = true;
-                    animator.SetBool("IsTrace", false);
-                    break;
-                case MonsterState.trace:
-                    //nvAgent.destination = playerTr.position;
-                    nvAgent.isStopped = false;
-                    animator.SetBool("IsAttack", false);
-                    animator.SetBool("IsTrace", true);
-                    break;
-                case MonsterState.attack:
-                    nvAgent.isStopped = true;
-                    animator.SetBool("IsAttack", true);
-                    break;
-            }
-            yield return null;
-        }
-    }
-    /*
-    void OnPlayerDie()
-    {
-        StopAllCoroutines();
-        nvAgent.isStopped = true;
-        animator.SetTrigger("IsPlayerDie");
-    }
-
-    private void OnCollisionEnter(Collision coll)
-    {
-        if (coll.gameObject.tag == "BULLET")
-        {
-            Destroy(coll.gameObject);
-            hp -= coll.gameObject.GetComponent<BulletCtrl>().damage;
-
-            if (hp <= 0)
-            {
-                MonsterDie();
-            }
-            else
-            {
-                animator.SetTrigger("IsHIt");
-            }
-        }
-    }
-
-    void MonsterDie()
-    {
-        StopAllCoroutines();
-
-        isDie = true;
-        monsterState = MonsterState.die;
-        nvAgent.isStopped = true;
-        animator.SetTrigger("IsDie");
-
-        gameObject.GetComponentInChildren<CapsuleCollider>().enabled = false;
-
-        foreach (Collider coll in gameObject.GetComponentsInChildren<SphereCollider>())
-        {
-            coll.enabled = false;
-        }
-    }
-    */
-    // Update is called once per frame
+    
     void Update()
     {
 

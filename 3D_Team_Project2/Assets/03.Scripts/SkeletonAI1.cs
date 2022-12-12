4dcfb1cf23913ba331903064419eb075afeefbed
RynT <clamper1223@naver.com>
2022-12-12 21:14:37 +0900

    Stage3-AI
    ai적용

---
+++ 3D_Team_Project2/Assets/03.Scripts/SkeletonAI1.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SkeletonAI1 : MonoBehaviour
+{
+    public enum MonsterState { idle, trace, attack, die };
+    public MonsterState monsterState = MonsterState.idle;
+    private Transform monsterTr;
+    private Transform playerTr;
+    private NavMeshAgent nvAgent;
+    private Animator animator;
+
+    public float traceDist = 30.0f;
+    public float attackDist = 2.0f;
+    private bool isDie = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterTr = this.gameObject.GetComponent<Transform>();
+        playerTr = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        nvAgent = this.gameObject.GetComponent<NavMeshAgent>();
+        animator = this.gameObject.GetComponent<Animator>();
+
+        StartCoroutine(this.CheckMonsterState());
+        StartCoroutine(this.MosterAction());
+    }
+
+    IEnumerator CheckMonsterState()
+    {
+        while (!isDie)
+        {
+            yield return new WaitForSeconds(0.2f);
+
+            float dist = Vector3.Distance(playerTr.position, monsterTr.position);
+
+            if (dist <= attackDist)
+            {
+                monsterState = MonsterState.attack;
+                GameObject.Find("Player").GetComponent<PlayerCtrl>().IsDie();
+                this.transform.position = GameObject.Find("SkelRespPos1").transform.position;
+                this.transform.rotation = GameObject.Find("SkelRespPos1").transform.rotation;
+            }
+            else if (dist <= traceDist)
+            {
+                monsterState = MonsterState.trace;
+            }
+            else
+            {
+                monsterState = MonsterState.idle;
+            }
+        }
+    }
+
+    IEnumerator MosterAction()
+    {
+        while (!isDie)
+        {
+            switch (monsterState)
+            {
+                case MonsterState.idle:
+                    nvAgent.isStopped = true;
+                    animator.SetBool("IsTrace", false);
+                    break;
+                case MonsterState.trace:
+                    nvAgent.destination = playerTr.position;
+                    nvAgent.isStopped = false;
+                    animator.SetBool("IsAttack", false);
+                    animator.SetBool("IsTrace", true);
+                    break;
+                case MonsterState.attack:
+                    nvAgent.isStopped = true;
+                    animator.SetBool("IsAttack", true);
+                    break;
+            }
+            yield return null;
+        }
+    }
+
+    void Update()
+    {
+
+    }
+}
